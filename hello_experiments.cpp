0b5fe4df5059599617f034984816164851688fc1
kaceyb <kacey.bennett@gmail.com>
2021-03-31 22:57:12 -0500

    experiments

---
+++ hello_experiments.cpp
@@ -0,0 +1,24 @@
+// hello.cpp: Kacey Bennett
+// Description: a program that prints the immortal saying "hello world"
+// many times
+
+#include <iostream>
+// need the following for setw() in some c++ implementations
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  for (int i = 0; i < 6; i++)
+  {
+    for (int r = 0; r < 4; r++)
+    {
+      cout << setw(17) << "Hello World! ";
+    }
+    cout << endl;
+  }
+
+  //cout << "Hello World!" << endl;
+  return 0;
+}
